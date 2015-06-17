c8ef57db7ce2098c4f526f6f8f6347810c359f00
Stephanie Siaw <siaw.stephanie@gmail.com>
2015-06-17 11:21:52 -0700

    Added test folders and added list of tests needed for menu

---
+++ test/com/twu/biblioteca/classes/MenuTest.java
@@ -0,0 +1,37 @@
+package com.twu.biblioteca.classes;
+
+import static org.junit.Assert.*;
+
+public class MenuTest {
+
+    //Should display all menu options
+    @Test
+    public void displaysOptions() {
+
+    }
+
+    //Should be able to select menu option
+    @Test
+    public void selectOption() {
+
+    }
+
+    //Should display error message if invalid option selected
+    @Test
+    public void returnsErrorIfOptionInvalid() {
+
+    }
+
+    //Should list all available books if option selected
+    @Test
+    public void displaysBookList() {
+
+    }
+
+    //Should list more book details if selected
+    @Test
+    public void displaysBookDetails() {
+
+    }
+
+}
